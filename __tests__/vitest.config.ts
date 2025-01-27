b46850081d5f9a6539c9daebb5d3016aa21ac18c
Augusto <augusto@coody.me>
2025-01-27 00:07:54 +0000

    first commit

---
+++ __tests__/vitest.config.ts
@@ -0,0 +1,12 @@
+
+import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    exclude: [...configDefaults.exclude, 'build/**/*'],
+    coverage: {
+      provider: 'v8',
+      exclude: [...coverageConfigDefaults.exclude, 'build/**/*']
+    },
+  },
+})
---
+++ dist/src/main.js
@@ -0,0 +1,3 @@
+console.log('aaa');
+export {};
+//# sourceMappingURL=main.js.map
